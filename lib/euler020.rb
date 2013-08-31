43bfd53ee58cef24a2d216a9367d4a3b37d54d9d
hoshinotsuyosh <guitarpopnot330+github@gmail.com>
2013-08-31 23:01:44 +0900

    solved 20

---
+++ lib/euler020.rb
@@ -0,0 +1,16 @@
+# coding: utf-8
+class Euler020
+  def self.solve
+    factorial(100).to_s.split(//).map(&:to_i).inject(&:+)
+  end
+  private
+  def self.factorial n
+    @factorial ||= []
+    @factorial[0] = 1
+    if @factorial[n] then
+      @factorial[n]
+    else
+      @factorial[n] = factorial(n-1) * n
+    end
+  end
+end
