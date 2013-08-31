dc8664b8ed6ed95cd0e219af3b21548baae4c47f
hoshinotsuyosh <guitarpopnot330+github@gmail.com>
2013-08-31 22:42:47 +0900

    solved 019

---
+++ lib/euler019.rb
@@ -0,0 +1,14 @@
+# coding: utf-8
+class Euler019
+  # これがRubyだ(やる気が無いわけではない)
+  def self.solve
+    require 'date'
+    c = 0
+    (1901..2000).each do |yyyy|
+      (1..12).each do |mm|
+	c += 1 if Date.new(yyyy,mm,1).sunday?
+      end
+    end
+    c
+  end
+end
