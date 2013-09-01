88a40dc8940b5fbb8b6befbb5bc7b920935318ed
hoshinotsuyosh <guitarpopnot330+github@gmail.com>
2013-09-02 01:08:32 +0900

    solved 30

---
+++ lib/euler030.rb
@@ -0,0 +1,11 @@
+# coding: utf-8
+class Euler030
+  def self.equals_sum_of_fifth_powers_of_their_digits? n
+    n == n.to_s.split(//).map{|e|e.to_i**5}.inject(&:+)
+  end
+  #9の5乗が60000以下で、これを5つ並べても高々300000までしか作れないので、
+  #6桁までの数値についてすべて調べれば十分
+  def self.solve
+    (2..999_999).select{|e|equals_sum_of_fifth_powers_of_their_digits? e}.inject(&:+)
+  end
+end
