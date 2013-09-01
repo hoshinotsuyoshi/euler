a4b6c8de59b97cfb1c08aa5e398100426c48088f
hoshinotsuyosh <guitarpopnot330+github@gmail.com>
2013-09-01 23:11:44 +0900

    solved 24 (hidoi)

---
+++ lib/euler024.rb
@@ -0,0 +1,31 @@
+# coding: utf-8
+class Euler024
+  def self.solve
+    array = (0..9).to_a
+    i = 0
+    array.each do |e1|
+      (array-[e1]).each do |e2|
+	(array-[e1,e2]).each do |e3|
+	  (array-[e1,e2,e3]).each do |e4|
+	    (array-[e1,e2,e3,e4]).each do |e5|
+	      (array-[e1,e2,e3,e4,e5]).each do |e6|
+		(array-[e1,e2,e3,e4,e5,e6]).each do |e7|
+		  (array-[e1,e2,e3,e4,e5,e6,e7]).each do |e8|
+		    (array-[e1,e2,e3,e4,e5,e6,e7,e8]).each do |e9|
+		      (array-[e1,e2,e3,e4,e5,e6,e7,e8,e9]).each do |e10|
+			i += 1
+			if i == 1_000_000 then
+			  return [e1,e2,e3,e4,e5,e6,e7,e8,e9,e10].join.to_i
+			end
+		      end
+		    end
+		  end
+		end
+	      end
+	    end
+	  end
+	end
+      end
+    end
+  end
+end
