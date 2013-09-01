1e7744139cfa0e0ec82df216187e172aa43991d7
hoshinotsuyosh <guitarpopnot330+github@gmail.com>
2013-09-02 00:49:10 +0900

    solved 28

---
+++ lib/euler028.rb
@@ -0,0 +1,16 @@
+# coding: utf-8
+class Euler028
+  def self.spiral n
+    @spiral    ||= []
+    @spiral[0] ||= 1
+    if @spiral[n]
+      @spiral[n]
+    else
+      x = 2*n+1
+      spiral(n-1) + (x**2)*4 - 6*(x-1)
+    end
+  end
+  def self.solve
+    spiral 500
+  end
+end
