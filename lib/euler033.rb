1e3a0aa7fb85b3a22ba8f83d73d01ea8131e4a69
hoshinotsuyosh <guitarpopnot330+github@gmail.com>
2013-09-02 02:37:14 +0900

    solved 33

---
+++ lib/euler033.rb
@@ -0,0 +1,19 @@
+# coding: utf-8
+class Euler033
+  def self.solve
+    array = []
+    # y/x
+    (11..99).each do |x|
+      next if x.to_s.split(//).last.to_i.zero?
+      next if x.to_s.split(//).uniq.size == 1
+      (11...x).each do |y|
+        next if y.to_s.split(//).last.to_i.zero?
+        next if y.to_s.split(//).uniq.size == 1
+	next if (x.to_s.split(//) + y.to_s.split(//)).uniq.size != 3
+	n = (x.to_s.split(//) & y.to_s.split(//))
+	array << [x,y] if (y.to_s.split(//)-n).first.to_f/(x.to_s.split(//)-n).first.to_i == y.to_f/x
+      end
+    end
+    array.map{|a|Rational(a.last,a.first)}.inject(&:*).denominator
+  end
+end
