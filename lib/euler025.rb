b1bacb8f067ab0cfe51caaff53358652b9e181ba
hoshinotsuyosh <guitarpopnot330+github@gmail.com>
2013-09-01 23:20:27 +0900

    solved 25

---
+++ lib/euler025.rb
@@ -0,0 +1,16 @@
+# coding: utf-8
+class Euler025
+  def self.fib n
+    @fib ||= {}
+    @fib[1] = 1
+    @fib[2] = 1
+    @fib[n] ||= fib(n-1) + fib(n-2)
+  end
+  def self.solve
+    n = 1
+    loop do
+      return n if fib(n).to_s.size == 1000
+      n += 1
+    end
+  end
+end
