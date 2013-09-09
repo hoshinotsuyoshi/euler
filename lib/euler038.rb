daefb393fa207d6aa061e4e87d2533cedaab348d
hoshinotsuyosh <guitarpopnot330+github@gmail.com>
2013-09-09 17:51:59 +0900

    solved 38

---
+++ lib/euler038.rb
@@ -0,0 +1,27 @@
+# coding: utf-8
+
+class Euler038
+  #元の数が5桁の場合、n>1だから、
+  #1,2を掛けたとたんに積の連結が10個以上になってしまうので、
+  #パンデジタル数が作れない。
+  #元の数(x)は4桁以内であると考えて良い。
+
+  def self.solve
+    tmp = []
+    def tmp.pandigital?
+      join.split(//).sort.map(&:to_i) == (1..9).to_a
+    end
+    pandigitals = []
+    (1..9999).each do |x|
+      tmp.clear 
+      (1..9).each do |i|
+	tmp << x * i
+	if tmp.join.size == 9
+	  pandigitals << tmp.join if tmp.pandigital?
+	end
+	break if tmp.join.size > 9
+      end
+    end
+    pandigitals.sort.last.to_i
+  end
+end
