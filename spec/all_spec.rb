c1aa1c0f8f3c25501c9a307baa4c8a31714041ce
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2014-11-29 19:34:03 +0900

    Use expect instead of should

--- spec/all_spec.rb
+++ spec/all_spec.rb
@@ -5,7 +5,7 @@ Dir.glob("lib/*.rb").each do |lib|
   euler = eval lib.sub("lib/","").sub(".rb","").capitalize
   describe euler do
     it 'return collect answer' do
-      euler.solve.should == answers[euler.to_s[-3,3].to_i-1]
+      expect(euler.solve).to eq(answers[euler.to_s[-3,3].to_i-1])
     end
   end
 end
